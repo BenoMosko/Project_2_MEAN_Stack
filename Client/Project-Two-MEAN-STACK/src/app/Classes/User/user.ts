5be412dab371452b57e2004fde1bdb5d9c02fb90
Beno Mosko <34980898+BenoMosko@users.noreply.github.com>
2021-07-19 12:53:14 +0300

    Add files via upload

---
+++ Client/Project-Two-MEAN-STACK/src/app/Classes/User/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    _id : Number = 0
+    Name : String = ""
+    Email : String =""
+    City : String = ""
+    Street : String = ""
+    Zipcode : Number = 0
+    Tasks : [] = []
+    Posts : [] = []
+}
