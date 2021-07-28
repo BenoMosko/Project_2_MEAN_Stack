3aeb4917a85bd38b89d7ee301419673e12712f3f
Beno Mosko <34980898+BenoMosko@users.noreply.github.com>
2021-07-28 13:22:40 +0300

    Add files via upload

--- Client/Project-Two-MEAN-STACK/src/app/Components/Users/users/users.component.ts
+++ Client/Project-Two-MEAN-STACK/src/app/Components/Users/users/users.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { User } from 'src/app/Classes/User/user';
+import { Service1Service } from 'src/app/Services/Service1/service1.service';
 
 @Component({
   selector: 'app-users',
@@ -10,16 +11,31 @@ import { User } from 'src/app/Classes/User/user';
 })
 export class UsersComponent implements OnInit {
 
-  constructor(private http : HttpClient) { }
+  constructor(private http : HttpClient, private service : Service1Service) { }
 
   sub : Subscription = new Subscription();
   users : User[] = [];
+  filteredUsers: User[] = [];
 
   ngOnInit(): void {
     this.http.get<User[]>("http://localhost:8000/api/Users").subscribe(data =>
     {
       this.users = data;
+      this.filteredUsers = data;      
     })
+  }
+
+  search(text : String)
+  {
+    if(text == "")
+    {
+      this.filteredUsers = [...this.users]
+    }
+    else
+    {
+      this.filteredUsers = this.users.filter(x => x.Name.toLowerCase().includes(text.toLowerCase()))  
+    }
+    
   }
 
 }
