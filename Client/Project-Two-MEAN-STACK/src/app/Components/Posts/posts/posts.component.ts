ae6f9fe5af546f4d1d2697403e2c72ca1c363f9e
Beno Mosko <34980898+BenoMosko@users.noreply.github.com>
2021-07-31 15:12:42 +0300

    Add files via upload

--- Client/Project-Two-MEAN-STACK/src/app/Components/Posts/posts/posts.component.ts
+++ Client/Project-Two-MEAN-STACK/src/app/Components/Posts/posts/posts.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/Classes/User/user';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http :HttpClient) { }
+
+  @Input()
+  userPost : User = new User();
+
+  sub : Subscription = new Subscription();
+  addPosts : Boolean = false;
 
   ngOnInit(): void {
+  }
+
+  updatePosts()
+  {
+    this.sub = this.http.put('http://localhost:8000/api/Users/' + this.userPost._id, this.userPost).subscribe((status) =>
+    {
+        alert(status);
+        window.location.reload();
+      });
+  }
+
+  update(title: String, newBody: String)
+  {
+    if(title != '' && newBody != '')
+    {
+      let obj = {Title: title, Body: newBody };
+      this.userPost.Posts.push(obj);
+      this.updatePosts();
+    }
+    else
+    {
+      alert("One or more fields are empty!")
+    }
+    
+  }
+
+  reload()
+  {
+    window.location.reload();
+  }
+
+  ngOnDestroy()
+  {
+    this.sub.unsubscribe();
   }
 
 }
